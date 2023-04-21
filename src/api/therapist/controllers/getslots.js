00b7584eac40a61d11a2ebbc02a8d0ce0b509e70
unknown <akashrobo2@gmail.com>
2023-04-21 06:47:01 +0530

    TS resolved

--- src/api/therapist/controllers/getslots.js
+++ src/api/therapist/controllers/getslots.js
@@ -1,13 +1,13 @@
 module.exports = {
   async findSlots(ctx) {
     const eventTsRes = await strapi.db.query("api::event.event").findMany({
-      select: ["therapist", "timeslot"],
+      select: ["therapist", "timeslot", "mode"],
     });
 
     const therapistTsRes = await strapi.db
       .query("api::therapist.therapist")
       .findMany({
-        select: ["name", "timeslot"],
+        select: ["name", "timeslot", "mode"],
       });
 
     const bookingTsRes = await strapi.db
@@ -26,6 +26,7 @@ module.exports = {
           eventTs.push({
             name: eventTsRes[i].therapist,
             timeslot: eventTsRes[i].timeslot[k].ts,
+            mode: eventTsRes[i].mode,
           });
         }
       }
@@ -37,6 +38,7 @@ module.exports = {
           therapistTs.push({
             name: therapistTsRes[i].name,
             timeslot: therapistTsRes[i].timeslot[k].ts,
+            mode: eventTsRes[i].mode,
           });
         }
       }
@@ -48,42 +50,83 @@ module.exports = {
     for (var p = 0; p < eventTs.length; p++) {
       for (var q = 0; q < therapistTs.length; q++) {
         if (eventTs[p].name === therapistTs[q].name) {
-          if (eventTs[p].timeslot === therapistTs[q].timeslot) {
-            resultTs.push({
-              name: eventTs[p].name,
-              ts: eventTs[p].timeslot,
-            });
+          if (eventTs[p].mode === "Offline") {
+            console.log("Same");
+            if (eventTs[p].timeslot === therapistTs[q].timeslot) {
+              resultTs.push({
+                name: eventTs[p].name,
+                ts: eventTs[p].timeslot,
+              });
+            }
           }
         }
       }
     }
 
-    for (var x = 0; x < bookingTsRes.length; x++) {
-      for (var y = 0; y < resultTs.length; y++) {
-        console.log(
-          new Date(bookingTsRes[x].date).toLocaleDateString("en-US", {
-            weekday: "long",
-          }),
-          ctx.params.day
-        );
+    var finalTs = [];
+
+    for (var y = 0; y < resultTs.length; y++) {
+      let matchFound = false;
+      for (var x = 0; x < bookingTsRes.length; x++) {
+        // console.log(
+        //   new Date(bookingTsRes[x].date).toLocaleDateString("en-US", {
+        //     weekday: "long",
+        //   }),
+        //   ctx.params.day
+        // );
         if (
           new Date(bookingTsRes[x].date).toLocaleDateString("en-US", {
             weekday: "long",
           }) === ctx.params.day
-        )
+        ) {
           if (bookingTsRes[x].therapist_name === resultTs[y].name) {
-            console.log(bookingTsRes[x].timeslot, resultTs[y].ts);
-            if (bookingTsRes[x].timeslot !== resultTs[y].ts) {
-              console.log({
-                name: resultTs[y].name,
-                ts: resultTs[y].ts,
-              });
+            // console.log(
+            //   bookingTsRes[x].therapist_name,
+            //   bookingTsRes[x].timeslot,
+            //   resultTs[y].name,
+            //   resultTs[y].ts
+            // );
+
+            if (bookingTsRes[x].timeslot === resultTs[y].ts) {
+              matchFound = true;
+              break;
             }
           }
+        }
+      }
+
+      if (!matchFound) {
+        finalTs.push({ name: resultTs[y].name, ts: resultTs[y].ts });
+        console.log(resultTs[y].ts);
       }
     }
 
-    // console.log("Result", resultTs);
-    ctx.send(resultTs);
+    var fts = finalTs.filter((obj, pos, arr) => {
+      return arr.map((mapObj) => mapObj.ts).indexOf(obj.ts) == pos;
+    });
+
+    ctx.send(fts);
   },
 };
+
+/*
+// create a new Date object for the current time
+var currentTime = new Date();
+
+// set the start and end times of the time slot
+var startTime = { hours: 9, minutes: 0 };  // 9:00 am
+var endTime = { hours: 17, minutes: 0 };   // 5:00 pm
+
+// extract the hour and minute values from the current time
+var currentHours = currentTime.getHours();
+var currentMinutes = currentTime.getMinutes();
+
+// check if the current time is within the time slot
+if ((currentHours > startTime.hours || (currentHours === startTime.hours && currentMinutes >= startTime.minutes))
+  && (currentHours < endTime.hours || (currentHours === endTime.hours && currentMinutes <= endTime.minutes))) {
+  console.log('The current time is within the time slot.');
+} else {
+  console.log('The current time is outside the time slot.');
+}
+
+*/
